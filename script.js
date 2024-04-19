38877ec893dd31493fff806f00d53af99b914795
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-04-19 23:23:19 +0300

    do the logic of console application

--- script.js
+++ script.js
@@ -1,13 +1,110 @@
-/*Напиши подробный пошаговый план для выполнения этой задачи:
+function createGameboard() {
+    const board = Array(9).fill(null);
+    const userPlayer = createPlayer();
+    const pcPlayer = createPCPlayer(userPlayer);
+    let currentPlayer = userPlayer.sign === 'X' ? userPlayer : pcPlayer;
 
-Мы делаем игру «Крестики-нолики», в которую можно играть в браузере!
+    function makeMove(index) {
+        if (this.checkCell(index) === false) {
+            return false;
+        }
+        board[index] = this.currentPlayer.sign;
+        let winnerSymbol = this.checkWinner();
+        if (winnerSymbol === false) {
+            this.swapUser();
+        }
+        else {
+            this.endGame(winnerSymbol);
+        }
+    }
 
-Настройте свой проект с помощью файлов HTML, CSS и Javascript и настройте репозиторий Git.
-Вы собираетесь хранить игровую доску в виде массива внутри объекта Gameboard, так что начните с этого! Ваши игроки также будут храниться в объектах, и вам, вероятно, понадобится объект, который будет управлять ходом самой игры.
-Ваша главная цель здесь — иметь как можно меньше глобального кода. Попробуйте запрятать как можно больше внутри фабрик. Если вам нужен только один экземпляр чего-либо (например, игровая доска, displayController и т. д.), то оберните фабрику внутри IIFE (шаблона модуля), чтобы ее нельзя было повторно использовать для создания дополнительных экземпляров.
-В этом проекте тщательно продумайте, где должен находиться каждый бит логики. Каждая маленькая часть функциональности должна вписываться в объекты игры, игрока или игрового поля. Позаботьтесь о том, чтобы разместить их в «логических» местах. Потратив немного времени на мозговой штурм, вы впоследствии сможете значительно облегчить себе жизнь!
-Если у вас возникли проблемы, прочтите «Построение дома изнутри наружу» — отличная статья, в которой изложен весьма применимый пример того, как вы можете подойти к реализации этого проекта, а также того, как вы можете организовать и структурировать свой код.
-Сначала сосредоточьтесь на том, чтобы установить работающую игру на консоль. Обязательно включите логику, проверяющую окончание игры! Вам следует проверять наличие всех выигрышных 3-х подряд и ничьих. Старайтесь не думать о DOM и HTML/CSS, пока ваша игра не заработает.
-Если у вас есть работающая консольная игра, создайте объект, который будет обрабатывать логику отображения/DOM. Напишите функцию, которая будет отображать содержимое массива игрового поля на веб-странице (на данный момент вы всегда можете просто заполнить массив игрового поля символами "X"s и "O"s, просто чтобы посмотреть, что происходит).
-Напишите функции, которые позволяют игрокам добавлять метки в определенное место на доске, взаимодействуя с соответствующими элементами DOM (например, позволяя игрокам щелкать по квадрату доски, чтобы разместить свой маркер). Не забывайте о логике, которая удерживает игроков от игры на уже занятых местах!
-Очистите интерфейс, чтобы игроки могли вводить свои имена, включите кнопку для запуска/перезапуска игры и добавьте элемент отображения, который показывает результаты по окончании игры!*/
+    function checkWinner() {
+        const winningConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let condition of winningConditions) {
+            const [a, b, c] = condition;
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                return board[a];
+            }
+        }
+
+        return false;
+
+    }
+
+    function swapUser() {
+        this.currentPlayer = this.currentPlayer === this.userPlayer ? this.pcPlayer : this.userPlayer;
+    }
+
+    function endGame(winnerSymbol) {
+        const message = winnerSymbol === this.userPlayer.sign ? 'You won!' : 'You lost!';
+        console.log(message);
+    }
+
+    function checkCell(index) {
+        if (this.board[index] === null) {
+            return true;
+        }
+        return false;
+    }
+
+    return {
+        board,
+        userPlayer,
+        pcPlayer,
+        currentPlayer,
+        makeMove,
+        checkWinner,
+        swapUser,
+        endGame,
+        checkCell
+    };
+}
+
+function createPlayer() {
+    const name = 'User';//getUserName() || 'User';
+    const sign = 'X';//getUserSign() || 'X';
+    return ({
+        name,
+        sign
+    });
+
+}
+
+function createPCPlayer(userPlayer) {
+    const name = 'Computer';
+    const sign = userPlayer.sign === 'X' ? 'O' : 'X';
+    return {
+        name,
+        sign
+    };
+
+}
+
+function generatePCMove() {
+    return Math.floor(Math.random() * 9) + 1;
+}
+
+function getUserMove() {
+    return prompt('Enter a number between 1 and 9:');
+}
+
+
+const gameboard = createGameboard();
+
+gameboard.makeMove(0);
+gameboard.makeMove(1);
+gameboard.makeMove(3);
+gameboard.makeMove(4);
+gameboard.makeMove(6);
+gameboard.makeMove(5);
+console.log(gameboard.board);
